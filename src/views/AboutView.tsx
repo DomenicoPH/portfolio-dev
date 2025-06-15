b29f300af72de0c38233cdc5742f1348f8a38df3
Domenico Pagano <dpaganoh@gmail.com>
2025-06-15 18:07:27 -0500

    deploy

--- src/views/AboutView.tsx
+++ src/views/AboutView.tsx
@@ -9,6 +9,10 @@ export default function AboutView(){
                     src='/me.jpg' alt="logo" width={1000} height={1000}
                 ></Image>
             </div>
+            <Image
+                    className='object-cover scale-220 ml-6'
+                    src='/me.jpg' alt="logo" width={1000} height={1000}
+                ></Image>
             <div className="w-1/2">
                 <h1 className="montserrat text-fuchsia-500 py-5">Welcome. This is Landing View</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, sint officia laudantium in incidunt animi exercitationem iure voluptates, repudiandae laborum quidem debitis, numquam illum consequuntur quasi minus omnis vero ea. Minima rerum error obcaecati. Voluptatum eaque accusantium hic adipisci recusandae, magnam architecto ipsum magni non excepturi molestiae enim quidem doloremque.</p>
